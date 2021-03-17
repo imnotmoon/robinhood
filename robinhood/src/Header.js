467e04b1d85f0197664369e77df5c9c613d76864
imnotmoon <imnotmoon@gmail.com>
2021-03-18 05:38:55 +0900

    build division + css for header

---
+++ robinhood/src/Header.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Logo from './robinhood.svg'
+import './Header.css'
+
+function Header() {
+    return (
+        <div className="header__wrapper">
+            {/* logo */}
+            <div class="header__logo">
+                <img src={Logo} width={25} />
+            </div>
+
+            {/* search */}
+            <div class="header__search">
+                <div class="header__searchContainer">
+                    <input placeholder="Search" type="text" />
+                </div>
+            </div>
+            {/* menuitems */}
+            <div class="header__menuItems">
+                <a href="#">Free Stocks</a>
+                <a href="#">Portfolio</a>
+                <a href="#">Cash</a>
+                <a href="#">Messages</a>
+                <a href="#">Account</a>
+            </div>
+        </div>
+    )
+}
+
+export default Header
