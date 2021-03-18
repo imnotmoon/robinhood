f1e353e5bc5691a4fe4659d36dc24760a5a636fb
imnotmoon <imnotmoon@gmail.com>
2021-03-19 03:59:59 +0900

    chart done with random data of year

--- robinhood/src/LineGraph.js
+++ robinhood/src/LineGraph.js
@@ -1,46 +1,75 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Line } from 'react-chartjs-2'
+import './LineGraph.css'
 
 function LineGraph() {
 
-    let data = [{
-        x: 10,
-        y: 20
-    }, {
-        x: 15,
-        y: 10
-    }, {
-        x: 12,
-        y: 4
-    }]
+    const [graphData, setGraphData] = useState([])
+
+    const createMockData = () => {
+        let data = []
+        let value = 50;
+        for (var i = 0; i < 366; i++) {
+            let date = new Date()
+            date.setDate(i)
+            value += Math.round((Math.random() < 0.5 ? 1 : 0) * Math.random() * 10)
+            data.push(({ x: date, y: value }))
+        }
+        setGraphData(data)
+    }
+
+    useEffect(() => {
+        createMockData();
+    }, [])
 
     return (
-        <div>
+        <div className="linegraph">
             <Line
                 data={{
                     datasets: [
                         {
                             type: "line",
-                            data: data,
+                            data: graphData,
                             backgroundColor: "black",
                             borderColor: "#5AC53B",
                             borderWidth: 2,
+                            fill: false,
                             pointBorderColor: "rgba(0,0,0,0)",
+                            pointBackgroundColor: "rgba(0,0,0,0)",
                             pointHoverBackgroundColor: "5AC53B",
                             pointHoverBorderColor: "#000000",
                             pointHoverBorderWidth: 4,
-                            pointHoverRadius: 6
+                            pointHoverRadius: 4
                         }
                     ]
                 }}
+
                 options={{
                     legend: false,
+                    maintainAspectRatio: false,
                     scales: {
+                        xAxes: [{
+                            type: "time",
+                            time: {
+                                format: "MM/DD/YY",
+                                tooltipFormat: "ll",
+                            },
+                            ticks: {
+                                display: false,
+                            },
+                        }],
                         yAxes: [{
+                            gridLines: {
+                                display: false,
+                            },
                             ticks: {
                                 display: false
                             }
                         }]
+                    },
+                    tooltips: {
+                        mode: "index",
+                        intersect: false,
                     }
                 }}
             />
